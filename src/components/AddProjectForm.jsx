d611bf63923037e121358832c4b812d4942fd1f9
Michael Shi <michaelshi03@gmail.com>
2022-02-11 11:11:12 -0800

    Implement basic styling for form & button components using styled-components

--- src/components/AddProjectForm.jsx
+++ src/components/AddProjectForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const AddProjectForm = ({ handleSubmit }) => {
   const [ title, setTitle ] = useState('');
@@ -7,46 +8,67 @@ const AddProjectForm = ({ handleSubmit }) => {
   const [ releaseYear, setReleaseYear] = useState('');
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label htmlFor="title">Project Title: </label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-      />
-      <label htmlFor="artist">Artist: </label>
-      <input
-        type="text"
-        id="artist"
-        name="artist"
-        value={artist}
-        onChange={e => setArtist(e.target.value)}
-      />
-      <label htmlFor="genre">Genre: </label>
-      <select
-        id="genre"
-        value={genre}
-        onChange={e => setGenre(e.target.value)}
-      >
-        <option value="rock">Rock</option>
-        <option value="pop">Pop</option>
-        <option value="hip-hop">Hip-hop</option>
-        <option value="electronic">Electronic</option>
-        <option value="other">Other</option>
-      </select>
-      <label htmlFor="releaseYear">Release Year: </label>
-      <input
-        type="text"
-        id="releaseYear"
-        name="releaseYear"
-        value={releaseYear}
-        onChange={e => setReleaseYear(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <FormWrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <FieldWrapper>
+          <label htmlFor="title">Project Title: </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </FieldWrapper>
+        <FieldWrapper>
+          <label htmlFor="artist">Artist: </label>
+          <input
+            type="text"
+            id="artist"
+            name="artist"
+            value={artist}
+            onChange={e => setArtist(e.target.value)}
+          />
+        </FieldWrapper>
+        <FieldWrapper>
+          <label htmlFor="genre">Genre: </label>
+          <select
+            id="genre"
+            value={genre}
+            onChange={e => setGenre(e.target.value)}
+          >
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="hip-hop">Hip-hop</option>
+            <option value="electronic">Electronic</option>
+            <option value="other">Other</option>
+          </select>
+        </FieldWrapper>
+        <FieldWrapper>
+          <label htmlFor="releaseYear">Release Year: </label>
+          <input
+            type="text"
+            id="releaseYear"
+            name="releaseYear"
+            value={releaseYear}
+            onChange={e => setReleaseYear(e.target.value)}
+          />
+        </FieldWrapper>
+        <FieldWrapper>
+          <button type="submit">Add Project</button>
+        </FieldWrapper>
+      </form>
+    </FormWrapper>
   );
 };
+
+const FormWrapper = styled.div`
+  background-color: #f2f2f2;
+  padding: 10px;
+`;
+
+const FieldWrapper = styled.span`
+  margin: 10px;
+`;
 
 export default AddProjectForm;
--- src/components/ListButton.jsx
+++ src/components/ListButton.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-const ListButton = ({ projects, handleClick }) => {
-  return (
-    <button onClick={handleClick}>
-      Display List
-    </button>
-  );
-};
+const ListButton = ({ projects, handleClick }) => (
+  <Button onClick={handleClick}>
+    View All Projects
+  </Button>
+);
+
+const Button = styled.button`
+  margin: 15px;
+`;
 
 export default ListButton;

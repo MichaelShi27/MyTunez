53280af5ce31e68dcd58e9190d1cf8246e96e4e2
Michael Shi <michaelshi03@gmail.com>
2022-02-11 19:46:06 -0800

    Modify styling & implement a header row with labels for the list

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,39 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ProjectsList = ({ projects }) => (
-  projects.map(({ title, artist, genre }, idx) => (
+const ProjectsList = ({ projects }) => (<>
+  <Row>
+    <TextWrapper label={'true'} type={'title'}>Project Title</TextWrapper>
+    <TextWrapper label={'true'} type={'artist'}>Artist</TextWrapper>
+    <TextWrapper label={'true'} type={'genre'}>Genre</TextWrapper>
+  </Row>
+  {projects.map(({ title, artist, genre }, idx) => (
     <Project key={idx} genre={genre}>
       <TextWrapper type={'title'}><em>{title}</em></TextWrapper>
       <TextWrapper type={'artist'}>{artist}</TextWrapper>
       <TextWrapper type={'genre'} genre={genre}>{genre}</TextWrapper>
     </Project>
-  ))
-);
+  ))}
+</>);
+
+const Row = styled.div`
+  margin: 3px;
+  width: 1000px;
+  padding: 3px;
+  background-color: #e0e0e0;
+`;
 
 const TextWrapper = styled.div`
   padding: 0 5px;
   // border: 1px solid red;
   display: inline-block;
+  text-align: ${({ type }) => type === 'genre' && 'center'};
   width: ${({ type }) => (
     type === 'title' ? '600px' :
     type === 'artist' ? '280px' :
     type === 'genre' ? '80px' :
     null
   )};
-  background-color: ${({ type, genre }) => (
+  background-color: ${({ genre, label }) => (
     genre === 'rock' ? 'rgb(255, 255, 49)' :
     genre === 'pop' ? 'rgb(255, 158, 242)' :
     genre === 'hip-hop' ? 'rgb(71, 250, 86);)' :
     genre === 'electronic' ? 'aqua' :
-    'white'
+    label ? '#e0e0e0' : 'white'
   )};
 `;
 
-const Project = styled.div`
-  margin: 3px;
-  width: 1000px;
-  padding: 3px;
+const Project = styled(Row)`
+  background-color: white;
   border: 1px solid gray;
 `;
 

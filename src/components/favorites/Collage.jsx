e403eb9f47141f5e3e12672f09033be2a49014d0
Michael Shi <michaelshi03@gmail.com>
2022-10-15 22:54:39 -0700

    Fix bug where 'No favorites' text appeared briefly when there are favorites

--- src/components/favorites/Collage.jsx
+++ src/components/favorites/Collage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ProjectImage from '../projectPage/ProjectImage';
 import { Loading } from '../styles.js';
@@ -6,17 +6,22 @@ import { Loading } from '../styles.js';
 const Collage = ({ favorites }) => {
   const [ loading, setLoading ] = useState(true);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (<>
-    {!favorites.length && (
+    {loading && <Loading>LOADING...</Loading>}
+    {!favorites.length && !loading && (
       <Loading style={{ color: '#e0e0e0', fontFamily: 'Helvetica' }}>
         No favorites added!
       </Loading>
     )}
-    {favorites.length > 0 && loading && <Loading>LOADING...</Loading>}
     <div style={{ marginTop: '20px' }}>
       {favorites.map(project => (
         <ProjectImage 
-          key={project.id}
+          key={project._id}
           project={project}
           onLoad={() => setLoading(false)}
           style={{
